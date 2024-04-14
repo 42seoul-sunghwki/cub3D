4e08155e7e63ecc986dfaec9f3085a7ee69b2b56
sunghwki <swkim12345@naver.com>
2024-04-14 21:47:09 +0900

    fix norm

--- src/bonus/mergesort_sprite_bonus.c
+++ src/bonus/mergesort_sprite_bonus.c
@@ -6,43 +6,36 @@
 /*   By: sunghwki <sunghwki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 15:09:32 by jacob             #+#    #+#             */
-/*   Updated: 2024/04/14 21:39:45 by sunghwki         ###   ########.fr       */
+/*   Updated: 2024/04/14 21:46:58 by sunghwki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	swap_sprite(t_sprite_node *a, t_sprite_node *b)
+void	merge_sort_sprite_node_divide(t_sprite_node **tmp, t_sprite_node **left,
+		t_sprite_node **right, int size)
 {
-	t_sprite_node	temp;
-
-	temp = *a;
-	*a = *b;
-	*b = temp;
-}
-
-t_sprite_node	**merge_sort_sprite_node(t_sprite_node **tmp, int size)
-{
-	t_sprite_node	**left;
-	t_sprite_node	**right;
 	int				i;
 	int				j;
 
-	//divide
 	if (size <= 1)
 		return (tmp);
-	left = (t_sprite_node **)malloc(sizeof(t_sprite_node *) * (size / 2));
-	right = (t_sprite_node **)malloc(sizeof(t_sprite_node *) * (size - size / 2));
 	i = -1;
 	while (++i < size / 2)
 		left[i] = tmp[i];
 	j = -1;
 	while (++j < size - size / 2)
 		right[j] = tmp[i + j];
-	//conquer
 	left = merge_sort_sprite_node(left, size / 2);
 	right = merge_sort_sprite_node(right, size - size / 2);
-	//merge
+}
+
+void	merge_sort_sprite_node_conquer(t_sprite_node **tmp,
+		t_sprite_node **left, t_sprite_node **right, int size)
+{
+	int				i;
+	int				j;
+
 	i = 0;
 	j = 0;
 	while (i < size / 2 && j < size - size / 2)
@@ -68,6 +61,19 @@ t_sprite_node	**merge_sort_sprite_node(t_sprite_node **tmp, int size)
 		tmp[i + j] = right[j];
 		j++;
 	}
+}
+
+t_sprite_node	**merge_sort_sprite_node(t_sprite_node **tmp, int size)
+{
+	t_sprite_node	**left;
+	t_sprite_node	**right;
+	int				i;
+	int				j;
+
+	left = (t_sprite_node **)malloc(sizeof(t_sprite_node *) * (size / 2));
+	right = (t_sprite_node **)malloc(sizeof(t_sprite_node *) * (size - size / 2));
+	merge_sort_sprite_node_divide(tmp, left, right, size);
+	merge_sort_sprite_node_conquer(tmp, left, right, size);
 	free(left);
 	free(right);
 	return (tmp);
